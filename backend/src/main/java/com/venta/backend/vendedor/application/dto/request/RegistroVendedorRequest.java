c949e63c648010dc5d438b2363df637a29a78135
Gerardo <gerarleonce-0703@outlook.es>
2025-11-13 16:48:57 -0500

    Creando dto's generales

--- backend/src/main/java/com/venta/backend/vendedor/application/dto/request/RegistroVendedorRequest.java
+++ backend/src/main/java/com/venta/backend/vendedor/application/dto/request/RegistroVendedorRequest.java
@@ -1,4 +1,30 @@
 package com.venta.backend.vendedor.application.dto.request;
 
-public class RegistroVendedorRequest {
+import com.venta.backend.vendedor.enums.SellerType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data // Getters, Setters, etc.
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegistroVendedorRequest { // ¡Clase en Español!
+
+    // --- Datos de RRHH (si es INTERNO) o manuales (si es EXTERNO) ---
+    private String dni;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String address;
+
+    // --- Datos de Gestión ---
+
+    // Le dice a la lógica qué estrategia usar
+    private SellerType sellerType;
+
+    // El ID de la sede a la que será asignado
+    private Long sellerBranchId;
 }
