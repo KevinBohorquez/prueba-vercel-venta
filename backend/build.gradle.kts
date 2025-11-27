cd06c3791a3000df2a694d98da1f31d6f8b89d09
Gerardo <gerarleonce-0703@outlook.es>
2025-11-27 11:09:55 -0500

    Cambios q no me acuerdo

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -33,6 +33,12 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("tk.mybatis:mapper-spring-boot-starter:5.0.1")
+    // MapStruct
+    implementation ("org.mapstruct:mapstruct:1.6.3")
+    annotationProcessor ("org.mapstruct:mapstruct-processor:1.6.3")
+    // Esto es clave para que Lombok y MapStruct funcionen juntos
+    annotationProcessor ("org.projectlombok:lombok-mapstruct-binding:0.2.0")
 }
 
 tasks.withType<Test> {
